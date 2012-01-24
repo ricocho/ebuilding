00c696dbc9ddc7bfa185074163cd7847556628ef
Your Name <ejgarm@att.net>
2012-01-24 15:08:29 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebuilding::Application.config.secret_token = '728a67e0ab4fac39c39521e219d2bc663b0e3fd0aba63f76d3536ecd5d539dfe20634a09ce674f33b47b175c28334d8a1378138a3aa6a8d1096dd0113aa218c6'
